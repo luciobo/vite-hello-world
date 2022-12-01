2607c929dc499b97def69a568c700077781fc93c
luciobo <luciobo@icloud.com>
2022-12-01 16:56:21 +0100

    inserito messaggio tramite vite

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,9 +1,27 @@
+<script>
+	export default {
+        
+
+    data() {
+        return {
+            messaggio: "la mia prima app in vite... "
+        };
+    },
+    methods: {
+    },
+
+    beforeMount() {
+    },
+
+	}
+</script>
+
+
+
 <template>
     <div>
-        
+        <h1>{{messaggio}}</h1>
+        <p>ecco il mio primo lavoro con vite</p>
     </div>
 </template>
 
-<script>
-
-</script>
